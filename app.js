335fd976568997655f54bb46ce0ed7eba702a95b
sujit gouda <sujit.gda997@gmail.com>
2025-06-28 09:06:12 +0530

    updated

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Vehicle Booking API');
+})
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/bookings', bookingRoutes);
 
